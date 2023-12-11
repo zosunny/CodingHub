3a89e6e0b3f59cd225eefd139c65bf3911a0e060
HaeRin <104357560+zosunny@users.noreply.github.com>
2023-12-12 00:04:30 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    
+    static ArrayList<Integer> list;
+    static int answer;
+    
+    // k진수로 변환하는 함수
+    public static void convert(int n, int k){
+        
+        list = new ArrayList<>();
+        while(n >= k){
+            // 나머지
+            list.add(n % k);
+            // 몫
+            n /= k;
+        }
+        list.add(n);
+    }
+
+    // 소수를 판별하는 함수
+    public static void isPrime(long tmp){
+        for(int i=2; i<=(int)Math.sqrt(tmp); i++){
+            if(tmp % i == 0){
+                return;
+            }
+        }
+        answer++;
+    }
+    
+    public int solution(int n, int k) {
+        
+        convert(n, k);
+        
+        // 0을 기준으로 자른 숫자 소수 판별
+        // 이때 뒤에서부터 탐색
+        long tmp = 0;
+        for(int i=list.size()-1; i>=-1; i--){
+            // 0이면 현재자리까지의 수가 소수인지 판별
+            if(i == -1 || list.get(i) == 0){
+                // System.out.println("tmp: " + tmp);
+                if(tmp != 0 && tmp != 1){
+                    isPrime(tmp);
+                }
+                tmp = 0;
+                // System.out.println("-----------");
+            }else{
+                tmp = tmp * 10 + list.get(i);
+                // System.out.println("i: " + i + ", tmp: " + tmp);
+            }
+        }
+        
+        return answer;
+    }
+}
