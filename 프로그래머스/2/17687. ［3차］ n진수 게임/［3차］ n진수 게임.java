fdaf74cf66eba70ddbc7651d6bffc48f5e532008
HaeRin <104357560+zosunny@users.noreply.github.com>
2023-12-28 22:46:43 +0900

    [level 2] Title: [3차] n진수 게임, Time: 6.56 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        
+        int len = 0;        // 현재 차례
+        int tube = p;       // 튜브 차례
+        
+        
+        // t*m 수 까지 게임 진행
+        for(int i=0; i<=t*m; i++){
+            // n진수로 변환
+            String tmp = Integer.toString(i, n).toUpperCase();
+            // 튜브 순서인지 확인
+            for(int j=0; j<tmp.length(); j++){
+                len++;
+                // 튜브 순서면
+                if(len == tube){
+                    answer += tmp.substring(j, j+1);
+                    if(answer.length() == t) break;
+                    // 다음 튜브 순서
+                    tube += m;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
