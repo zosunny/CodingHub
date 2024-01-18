fc90cfe19fa736a2d778e9febeaa0387cba91270
HaeRin <104357560+zosunny@users.noreply.github.com>
2024-01-18 18:14:10 +0900

    [level 1] Title: 폰켓몬, Time: 3.67 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,24 +1,15 @@
 import java.util.*;
 
 class Solution {
-    
-    static int len;
-    static int answer;
-    static Set<Integer> set;
-    
-    public int solution(int[] nums) {
-        len = nums.length;
 
-        set = new HashSet<>();
+    public int solution(int[] nums) {
+        int len = nums.length;
+
+        Set<Integer> set = new HashSet<>();
         for(int i=0; i<len; i++){
             set.add(nums[i]);
         }
         
-        int v = set.size();
-        int N = len/2;
-        if(v <= N) answer = v;
-        else answer = N;
-        
-        return answer;
+        return set.size() <= len/2 ? set.size() : len/2;
     }
 }
