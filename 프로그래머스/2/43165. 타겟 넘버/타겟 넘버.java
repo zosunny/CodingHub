c178f2832606bb225ac56050760f7c8176a3a195
HaeRin <104357560+zosunny@users.noreply.github.com>
2024-05-01 22:59:25 +0900

    [level 2] Title: 타겟 넘버, Time: 1.08 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,43 +1,38 @@
+import java.util.*;
+
 class Solution {
     
-    static int len;
-    static int[] input;
     static int answer;
+    static int len;
     
-    public static void calc(int[] numbers, int target){
+    public static int calc(boolean[] op, int[] numbers){
         int tmp = 0;
         for(int i=0; i<len; i++){
-            // + 연산자일 때
-            if(input[i] == 0){
-                tmp += numbers[i];
-            }
-            // - 연산자일 때
-            else{
-                tmp -= numbers[i];
-            }
+            // 연산자가 + 인 경우
+            if(op[i]) tmp += numbers[i];
+            // 연산자가 - 인 경우
+            else tmp -= numbers[i];
         }
-        // 계산 결과가 타겟 넘버와 같으면
-        if(tmp == target) answer++;
+        return tmp;
     }
     
-    public static void permu(int cnt, int[] numbers, int target){
+    public static void subset(int cnt, boolean[] select, int[] numbers, int target){
         if(cnt == len){
-            // 선택한 연산자로 계산
-            calc(numbers, target);
+            if(calc(select, numbers) == target) answer++;
             return;
         }
-        for(int i=0; i<2; i++){
-            input[cnt] = i;
-            permu(cnt+1, numbers, target);
-        }
+        select[cnt] = true;
+        subset(cnt+1, select, numbers, target);
+        select[cnt] = false;
+        subset(cnt+1, select, numbers, target);
     }
     
     public int solution(int[] numbers, int target) {
-        len = numbers.length;
         
-        // +, - 중복 순열
-        input = new int[len];
-        permu(0, numbers, target);
+        len = numbers.length;   // 숫자 개수
+        
+        boolean[] select = new boolean[len];
+        subset(0, select, numbers, target);
         
         return answer;
     }
