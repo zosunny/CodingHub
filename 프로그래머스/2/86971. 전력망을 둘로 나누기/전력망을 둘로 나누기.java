44fcf5ddfa2ffdb7334adb5ee16dd5d7f7ad83b0
HaeRin <104357560+zosunny@users.noreply.github.com>
2024-05-03 23:29:16 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 130.30 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,51 +2,75 @@ import java.util.*;
 
 class Solution {
     
-    static int cnt;
-    static ArrayList<Integer>[] graph;
+    static int answer = Integer.MAX_VALUE;
+    static int len;
+    static boolean[] select;
     static boolean[] visited;
+    static List<Integer>[] list;
     
-    // 하나의 전력망에 연결된 송전탑 개수 카운트하는 함수
     public static void dfs(int x){
         visited[x] = true;
-        for(int i=0; i<graph[x].size(); i++){
-            int now = graph[x].get(i);
+        for(int i=0; i<list[x].size(); i++){
+            int now = list[x].get(i);
             if(visited[now]) continue;
-            cnt++;
             dfs(now);
+        }
+    }
+    
+    public static void combi(int cnt, int start, int n, int[][] wires){
+        if(cnt == 1){
+            // 인접리스트 생성
+            list = new ArrayList[n+1];
+            for(int i=0; i<n+1; i++){
+                list[i] = new ArrayList<>();
+            }
+            for(int i=0; i<len; i++){
+                if(select[i]) {
+                    System.out.println(wires[i][0] + ", " + wires[i][1] + " 끊음");
+                    continue;
+                }
+                int x = wires[i][0];
+                int y = wires[i][1];
+                list[x].add(y);
+                list[y].add(x);
+            }
+            for(int i=0; i<n+1; i++){
+                System.out.print("[" + i + "]: ");
+                for(int j=0; j<list[i].size(); j++){
+                    System.out.print(list[i].get(j) + " ");
+                }
+                System.out.println();
+            }
+            visited = new boolean[n+1];
+            dfs(1);
+            int ans = 0;
+            for(boolean x : visited) {
+                System.out.print(x + " ");
+                if(x) {
+                    ans++;
+                }
+            }
+            System.out.println();
+            answer = Math.min(answer, Math.abs(n - 2*ans));
+            System.out.println("차이: " + Math.abs(n - ans));
+            System.out.println("---------------");
+            return;
+        }
+        for(int i=start; i<len; i++){
+            if(select[i]) continue;
+            select[i] = true;
+            combi(cnt+1, i+1, n, wires);
+            select[i] = false;
         }
     }
     
     public int solution(int n, int[][] wires) {
         
-        int answer = Integer.MAX_VALUE;
-        int len = wires.length;
+        len = wires.length;
         
-        // 하나씩 전선을 끊으며 2차원 그래프 생성
-        for(int i=0; i<len; i++){
-            graph = new ArrayList[n+1];
-            // 2차원 그래프 개별 초기화
-            for(int p=0; p<n+1; p++){
-                graph[p] = new ArrayList<>();
-            }
-            int x = 0;
-            for(int j=0; j<len; j++){
-                // i번째 송전탑 연결 무시
-                if(j == i) continue;
-                // 송전탑 연결
-                int a = wires[j][0];
-                int b = wires[j][1];
-                graph[a].add(b);
-                graph[b].add(a);
-                x = a;
-            }
-            // 해당 전력망으로 연결된 송전탑 개수 구하기
-            cnt = 1;
-            visited = new boolean[n+1];
-            dfs(x);
-            // 두 전력망의 송전탑 차의 최솟값
-            answer = Math.min(answer, Math.abs(2*cnt - n));
-        }
+        select = new boolean[len];
+        combi(0, 0, n, wires);
+        
         return answer;
     }
 }
