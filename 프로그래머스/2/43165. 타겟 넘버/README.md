67bece5253b1e6d13cdf22e19b45448a8cff09f0
HaeRin <104357560+zosunny@users.noreply.github.com>
2024-07-14 00:11:49 +0900

    [level 2] Title: 타겟 넘버, Time: 52.49 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 78.7 MB, 시간: 1.08 ms
+메모리: 85.8 MB, 시간: 52.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 01일 22:59:22
+2024년 07월 14일 00:11:47
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -4,35 +4,34 @@ class Solution {
     
     static int answer;
     static int len;
+    static boolean[] select;
     
-    public static int calc(boolean[] op, int[] numbers){
-        int tmp = 0;
+    public static void calc(int[] numbers, int target){
+        int ans = 0;
         for(int i=0; i<len; i++){
-            // 연산자가 + 인 경우
-            if(op[i]) tmp += numbers[i];
-            // 연산자가 - 인 경우
-            else tmp -= numbers[i];
+            if(select[i]) ans += numbers[i];
+            else ans -= numbers[i];
         }
-        return tmp;
+        // 타겟넘버가 맞으면
+        if(ans == target) answer++;
     }
     
-    public static void subset(int cnt, boolean[] select, int[] numbers, int target){
+    public static void subset(int cnt, int[] numbers, int target){
         if(cnt == len){
-            if(calc(select, numbers) == target) answer++;
+            calc(numbers, target);
             return;
         }
         select[cnt] = true;
-        subset(cnt+1, select, numbers, target);
+        subset(cnt+1, numbers, target);
         select[cnt] = false;
-        subset(cnt+1, select, numbers, target);
+        subset(cnt+1, numbers, target);
     }
     
     public int solution(int[] numbers, int target) {
         
-        len = numbers.length;   // 숫자 개수
-        
-        boolean[] select = new boolean[len];
-        subset(0, select, numbers, target);
+        len = numbers.length;
+        select = new boolean[len];
+        subset(0, numbers, target);
         
         return answer;
     }
