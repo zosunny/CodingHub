b374f3adae89ac5e5682bd82cb22e6d075ec1c99
HaeRin <104357560+zosunny@users.noreply.github.com>
2024-07-14 11:08:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.86 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,21 +2,20 @@ import java.util.*;
 
 class Solution {
     
+    static int answer = Integer.MAX_VALUE;
+    static int N, M;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
     static class Point{
-        int x, y, dis;
+        int x, y;
+        int dis;
         Point(int x, int y, int dis){
             this.x = x;
             this.y = y;
             this.dis = dis;
         }
     }
-    
-    static int answer;
-    static int N;
-    static int M;
-    static int minDis = Integer.MAX_VALUE;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
     
     public static void bfs(int x, int y, int[][] maps){
         Queue<Point> q = new LinkedList<>();
@@ -25,19 +24,21 @@ class Solution {
         visited[x][y] = true;
         while(!q.isEmpty()){
             Point p = q.poll();
-            // System.out.println("시작점: " + p.x + ", " + p.y + " => " + p.dis);
-            // 도착했을 때 빠른 길 갱신
-            if(p.x == N-1 && p.y == M-1) {
-                minDis = Math.min(minDis, p.dis);
-                // System.out.println("도착");
+            // 상대 팀 진영에 도착한 경우
+            if(p.x == N-1 && p.y == M-1){
+                // 최단거리로 스위치
+                answer = Math.min(answer, p.dis);
             }
+            // 4방 탐색
             for(int i=0; i<4; i++){
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
                 int ndis = p.dis + 1;
-                if(nx<0 || ny<0 || nx>=N || ny>=M || maps[nx][ny]==0 || visited[nx][ny]) continue;
-                if(ndis >= minDis) continue;
-                // System.out.println(nx + ", " + ny + " => " + ndis);
+                // 못지나가는 경우
+                if(nx<0 || ny<0 || nx>=N || ny>=M || visited[nx][ny] || maps[nx][ny]==0) continue;
+                // 브루트포스 - 이동거리가 이미 최종 최단거리를 넘은 경우
+                if(ndis >= answer) continue;
+                // 다음 위치로 진행
                 q.add(new Point(nx, ny, ndis));
                 visited[nx][ny] = true;
             }
@@ -51,6 +52,6 @@ class Solution {
         
         bfs(0, 0, maps);
         
-        return minDis == Integer.MAX_VALUE ? -1 : minDis;
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
 }
