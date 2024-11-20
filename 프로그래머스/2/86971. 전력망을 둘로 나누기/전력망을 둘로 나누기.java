c799e7a2fc21a7ec78c046266a840f55f6430529
HaeRin <104357560+zosunny@users.noreply.github.com>
2024-11-20 15:32:45 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.14 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,48 +2,41 @@ import java.util.*;
 
 class Solution {
     
-    static int len;
-    static List<Integer>[] list;
-    static boolean[] visited;
-    static int cnt;
     static int answer = Integer.MAX_VALUE;
+    static int cnt = 1;
+    static boolean[] visited;
+    static List<Integer>[] list;
     
     public static void dfs(int x){
         visited[x] = true;
         for(int i=0; i<list[x].size(); i++){
             int now = list[x].get(i);
-            if(!visited[now]) {
-                dfs(now);
-                cnt++;
-            }
+            if(visited[now]) continue;
+            cnt++;
+            dfs(now);
         }
     }
     
     public int solution(int n, int[][] wires) {
-        len = wires.length;
+        int len = wires.length;
         
         for(int i=0; i<len; i++){
             list = new ArrayList[n+1];
-            for(int j=0; j<n+1; j++){
-                list[j] = new ArrayList<>();
+            for(int k=0; k<n+1; k++){
+                list[k] = new ArrayList<>();
             }
-            // 하나씩 끊어서 연결해보기
-            int a = 0;
+            cnt = 1;
             for(int j=0; j<len; j++){
                 if(i == j) continue;
                 int x = wires[j][0];
                 int y = wires[j][1];
                 list[x].add(y);
                 list[y].add(x);
-                a = x;
             }
-            // 해당 전력망으로 송전탑 차 구하기
             visited = new boolean[n+1];
-            cnt = 1;
-            dfs(a);
-            answer = Math.min(answer, Math.abs(2*cnt - n));
+            dfs(1);
+            answer = Math.min(answer, Math.abs(n - 2 * cnt));
         }
-        
         return answer;
     }
 }
