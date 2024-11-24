6fffab9e5d7abfaf2699c45d8e8e84df05a448b0
HaeRin <104357560+zosunny@users.noreply.github.com>
2024-11-24 13:10:08 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 128 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            // 초기값은 자기자신
+            dp[i] = arr[i];
+        }
+        // 또 여기서 실수함 ㅠ
+        int ans = dp[0];
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                // 이전 값에서 현재 값보다 작은 값이 있으면
+                if(arr[j] < arr[i]) dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        System.out.println(ans);
+    }
+}
