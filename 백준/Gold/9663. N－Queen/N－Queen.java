e588df57fe796d51d45b49c6db72d494fb88483e
rin <104357560+zosunny@users.noreply.github.com>
2025-02-03 16:48:01 +0900

    [Gold IV] Title: N-Queen, Time: 4756 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,46 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-	static int N;
-	static int cnt;
-	
-	static int[] col;
-	
-	public static boolean available(int row) {
-		for(int i=1; i<row; i++) {
-			if(col[i]==col[row] || Math.abs(col[i]-col[row])==row-i) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	public static void set(int row) {
-		// 이전까지의 퀸 유망성 체크 필요
-		if(!available(row-1)) return;
-		
-		// 기저조건
-		if(row == N+1) {
-			cnt++;
-			return;
-		}
-		
-		// 모든 열에 대해 넣어보고 재귀돌며 적합성을 판별
-		for(int i=1; i<N+1; i++) {
-			col[row] = i;			// 현재 들어온 행의 퀸에 열은 1~N 중에 넣어보고 재귀를 통해 적합한 위치에 들어감
-			set(row+1);
-		}
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		
-		col = new int[N+1];
-		
-		set(1);	// 0은 그냥 두고 1넣고 이전 퀸의 유망성 체크
-		
-		System.out.println(cnt);
-	}
+
+    static int n;
+    static int ans;
+    static int[] visited;
+
+    public static boolean check(int x, int y){
+        for(int i=1; i<n+1; i++){
+            // 열 확인
+            if(visited[i] == y) return false;
+            // 대각선 확인
+            if((x - i) == Math.abs(y - visited[i])) return false;
+        }
+        return true;
+    }
+
+    public static void dfs(int cnt){
+        if(cnt == n+1){
+            ans++;
+            return;
+        }
+        for(int i=1; i<n+1; i++){
+            if(check(cnt, i)){
+                visited[cnt] = i;
+                dfs(cnt+1);
+                visited[cnt] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        visited = new int[n+1];
+        dfs(1);
+
+        System.out.println(ans);
+    }
 }
