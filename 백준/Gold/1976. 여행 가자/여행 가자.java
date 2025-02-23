bdf200d0de53ac42f8da90db58e4ca9ebc5c5d36
rin <104357560+zosunny@users.noreply.github.com>
2025-02-23 17:52:51 +0900

    [Gold IV] Title: 여행 가자, Time: 172 ms, Memory: 18560 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -4,66 +4,52 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int M;
-	static int[] set;
-	
-	public static int find(int x) {
-		// 인덱스와 그 값이 같으면 최고 대표
-		if(x == set[x]) {
-			return x;
-		}
-		// 아니면 다시 대표 찾아서 현재 x의 대표로 교체
-		else {
-			return set[x] = find(set[x]);
-		}
-	}
-	
-	public static void union(int a, int b) {
-		// 각각의 대표 찾기
-		a = find(a);
-		b = find(b);
-		
-		// 둘의 대표가 다르면 합병
-		if(a != b) {
-			set[b] = a;
-		}
-	}
 
-	public static void main(String[] args) throws IOException {
-		 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());	// 도시의 수
-		M = Integer.parseInt(br.readLine());	// 여행 계획 도시의 수
-		
-		// 도시별 대표 생성
-		set = new int[N+1];
-		for(int i=1; i<N+1; i++) {
-			set[i] = i;
-		}
-		
-		// union-find
-		for(int i=1; i<N+1; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<N+1; j++) {
-				if(Integer.parseInt(st.nextToken()) == 1) {
-					union(i, j);
-				}
-			}
-		}
-		
-		// 계획된 도시들의 대표도시가 같으면 연결된 도시.
-		st = new StringTokenizer(br.readLine());
-		int a = find(Integer.parseInt(st.nextToken()));
-		for(int i=1; i<M; i++) {
-			if(find(Integer.parseInt(st.nextToken())) != a) {
-				System.out.println("NO");
-				return;
-			}
-		}
-		System.out.println("YES");		
-	}
+    static int n, m;
+    static int[] set;
+
+    public static int find(int x){
+        if(set[x] == x) return x;
+        else return set[x] = find(set[x]);
+    }
+
+    public static void union(int i, int j){
+        int a = find(i);
+        int b = find(j);
+
+        if(a < b) set[b] = a;
+        else if(a > b) set[a] = b;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        set = new int[n+1];
+        for(int i=1; i<n+1; i++){
+            set[i] = i;
+        }
+
+        for(int i=1; i<n+1; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<n+1; j++){
+                if(Integer.parseInt(st.nextToken()) == 1){
+                    union(i, j);
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int a = find(Integer.parseInt(st.nextToken()));
+        for(int i=1; i<m; i++){
+            if(a != find(Integer.parseInt(st.nextToken()))){
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
 }
