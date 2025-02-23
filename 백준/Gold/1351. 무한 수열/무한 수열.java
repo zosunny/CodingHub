abd927416d33c62a752739c78a3a10247a745cec
rin <104357560+zosunny@users.noreply.github.com>
2025-02-23 15:51:25 +0900

    [Gold V] Title: 무한 수열, Time: 100 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.java
+++ 백준/Gold/1351. 무한 수열/무한 수열.java
@@ -6,19 +6,16 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static long p, q;
     static Map<Long, Long> map = new HashMap<>();
 
-    static long calc(long x){
-        if(x == 0) return 1;
-        if(map.containsKey(x)) return map.get(x);
+    public static long calc(long n){
+        if(map.containsKey(n)) return map.get(n);
 
-        long a = (long)Math.floor(x / p);
-        long b = (long)Math.floor(x / q);
+        long tmp = calc(n/p) + calc(n/q);
+        map.put(n, tmp);
 
-        map.put(x, calc(a) + calc(b));
-        return map.get(x);
+        return tmp;
     }
 
     public static void main(String[] args) throws IOException {
@@ -29,6 +26,7 @@ public class Main {
         p = Long.parseLong(st.nextToken());
         q = Long.parseLong(st.nextToken());
 
-        System.out.println(calc(n));
+        map.put(0L, 1L);
+        System.out.println(calc(n));;
     }
 }
