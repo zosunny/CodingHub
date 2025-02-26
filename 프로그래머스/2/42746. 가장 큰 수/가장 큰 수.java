03db3243ee4a745bf2f448c2aba9127a951ecf2b
rin <104357560+zosunny@users.noreply.github.com>
2025-02-27 00:42:41 +0900

    [level 2] Title: 가장 큰 수, Time: 2788.94 ms, Memory: 388 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
+/*
+    개수 <= 100,000
+    1. 순열, 백트래킹 -> n! 안됨
+    2. 정렬 -> 두개를 문자열로 더한 값을 비교? -> n
+*/
+
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        int len = numbers.length;
-        String[] str = new String[len];
         
-        // 주어진 배열을 문자열로 변환
-        for(int i=0; i<len; i++){
-            str[i] = Integer.toString(numbers[i]);
+        int n = numbers.length;
+        
+        String[] arr = new String[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.toString(numbers[i]);
         }
-        // 내림차순 정렬
-        Arrays.sort(str, (o1, o2) -> (o2+o1).compareTo(o1+o2));
         
-        // 정수 이어붙이기
-        for(int i=0; i<len; i++){
-            // 만약 앞이 쭉 0이면
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        for(int i=0; i<n; i++){
             if(answer.equals("0")) answer = "";
-            answer += str[i];
+            answer += arr[i];
         }
         
         return answer;
