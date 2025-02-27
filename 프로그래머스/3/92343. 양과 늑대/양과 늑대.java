2e97194c5291e18d55c3dc7ac94ba897a3bc1d55
rin <104357560+zosunny@users.noreply.github.com>
2025-02-27 14:56:37 +0900

    [level 3] Title: 양과 늑대, Time: 3.84 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     
-    static int n;
     static int ans;
     static List<Integer>[] list;
     
@@ -11,41 +10,39 @@ class Solution {
         
         for(int i=0; i<node.size(); i++){
             
-            int n = node.get(i);
+            List<Integer> next = new ArrayList<>(node);     // for문 안에서 생성해서 dfs 경로에서 독립적으로 사용되게 해야함
             
-            // 다음 탐색 노드 생성
-            List<Integer> next = new ArrayList<>(node);
-            
-            // 현재 노드가 양이면
-            if(info[n] == 0){
-                next.remove(i);         // 다음 탐색할 때 제거
-                next.addAll(list[n]);   // 탐색 리스트에 자식 노드 추가
+            int now = node.get(i);
+            // 만약 현재 노드가 양이면
+            if(info[now] == 0){
+                next.remove(i);             // 탐색할 노드에서 현재 노드는 지우고
+                next.addAll(list[now]);     // 현 노드의 자식노드 모두 탐색 노드에 넣고
                 dfs(next, sheep+1, wolf, info);
             }
-            // 늑대지만 양의 수보다 적으면
+            // 현재 노드가 늑대지만 양의 수보다 적으면
             else if(wolf + 1 < sheep){
-                next.remove(i);         // 다음 탐색할 때 제거
-                next.addAll(list[n]);   // 탐색 리스트에 자식 노드 추가
+                next.remove(i);
+                next.addAll(list[now]);
                 dfs(next, sheep, wolf+1, info);
             }
-            // 그외(늑대 수가 양보다 크거나 같은 경우) 다음 노드 탐색
         }
     }
     
     public int solution(int[] info, int[][] edges) {
         
-        n = info.length;
+        int n = info.length;
+        int m = edges.length;
         
         list = new ArrayList[n];
         for(int i=0; i<n; i++){
             list[i] = new ArrayList<>();
         }
         
-        for(int[] e : edges){
-            list[e[0]].add(e[1]);
+        for(int i=0; i<m; i++){
+            list[edges[i][0]].add(edges[i][1]);
         }
         
-        // 탐색할 노드만 담는 새로운 리스트 생성
+        // 탐색할 노드를 담는 리스트 생성
         List<Integer> node = new ArrayList<>();
         node.add(0);
         
