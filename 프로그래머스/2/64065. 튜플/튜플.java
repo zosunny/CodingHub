73fc713376dc0cde9bc320065a0c59c313e242a9
rin <104357560+zosunny@users.noreply.github.com>
2025-02-28 21:19:34 +0900

    [level 2] Title: 튜플, Time: 61.94 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,46 +1,31 @@
 import java.util.*;
 
+/*
+    중복 가능, 순서 다르면 다른 튜플
+*/
+
 class Solution {
-    
-    static Set<String> set;
-    
     public int[] solution(String s) {
-        int[] answer = {};
         
-        // 양쪽 {{}} 제거
-        s = s.substring(2, s.length()-2);
-        // System.out.println(s);
-        String[] arr = s.split("},\\{");
-        // System.out.println("-----정렬 전-----");
-        // for(String x : arr){
-        //     System.out.print(x + " | ");
-        // }
-        // System.out.println();
+        s = s.substring(2, s.length()-2).replace("},{", "-");
+        String[] arr = s.split("-");
         
-        // 문자열 길이로 정렬
-        Arrays.sort(arr, (String s1, String s2) -> s1.length() - s2.length());
-        // System.out.println("-----정렬 후-----");
-        // for(String x : arr){
-        //     System.out.print(x + " | ");
-        // }
-        // System.out.println();
+        Arrays.sort(arr, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
         
-        // set과 answer에 담기
-        set = new HashSet<>();
-        answer = new int[arr.length];
-        int cnt = 0;
-        for(int i=0; i<arr.length; i++){
-            String[] tmp = arr[i].split(",");
-            // set에 없으면 answer에 담고 set에도 넣어주기
-            for(String x : tmp){
-                if(!set.contains(x)){
-                    answer[cnt++] = Integer.parseInt(x);
-                    set.add(x);
+        Set<Integer> set = new HashSet<>();
+        
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        
+        for(String str : arr){
+            for(String num : str.split(",")){
+                int now = Integer.parseInt(num);
+                if(!set.contains(now)){
+                    set.add(now);
+                    answer[idx++] = now;
                 }
             }
-            
         }
-        
         return answer;
     }
 }
