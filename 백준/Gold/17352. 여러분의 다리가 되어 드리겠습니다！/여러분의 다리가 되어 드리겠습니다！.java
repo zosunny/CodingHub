31220f381076c0c34c2e9adb5a18f228149485d1
rin <104357560+zosunny@users.noreply.github.com>
2025-04-24 14:45:47 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 472 ms, Memory: 81236 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
@@ -6,42 +6,42 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int n;
+    static int[] arr;
     static int[] set;
 
     public static int find(int x){
         if(set[x] == x) return x;
-        else return set[x] = find(set[x]);
+        else return find(set[x]);
     }
-
     public static void union(int x, int y){
         int a = find(x);
         int b = find(y);
         if(a < b) set[b] = a;
-        else if(a > b) set[a] = b;
+        else set[a] = b;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
 
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n+1];
         set = new int[n+1];
-        for(int i=1; i<n+1; i++){
+        for(int i=0; i<n+1; i++){
             set[i] = i;
         }
 
         for(int i=0; i<n-2; i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            union(a, b);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            union(x, y);
         }
 
-        StringBuilder sb = new StringBuilder();
         sb.append(1 + " ");
-
         int tmp = find(1);
-        for(int i=2; i<n+1; i++){
+        for(int i=1; i<n+1; i++){
             if(find(i) != tmp) {
                 sb.append(i);
                 break;
