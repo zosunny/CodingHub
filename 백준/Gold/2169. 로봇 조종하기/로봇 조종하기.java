bd62fbadfe92a2c399d41507b8a02a5fe5f17c1c
rin <104357560+zosunny@users.noreply.github.com>
2025-04-24 21:12:23 +0900

    [Gold II] Title: 로봇 조종하기, Time: 552 ms, Memory: 92228 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -4,18 +4,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n, m;
-    static int[][] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n][m];
+        int[][] arr = new int[n][m];
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<m; j++){
@@ -24,37 +20,34 @@ public class Main {
         }
 
         int[][] dp = new int[n][m];
-        dp[0][0] = arr[0][0];
 
-        // 맨 윗줄 누적합
-        for(int i=1; i<m; i++){
-            dp[0][i] = dp[0][i-1] + arr[0][i];
+        // 맨 윗줄 오른쪽으로 누적합
+        dp[0][0] = arr[0][0];
+        for(int j=1; j<m; j++){
+            dp[0][j] = dp[0][j-1] + arr[0][j];
         }
 
-
+        // 위에서부터 내려오면서 좌우 비교
         for(int i=1; i<n; i++){
-
-            // 위에서부터 누적합
+            // 내려오기
             for(int j=0; j<m; j++){
-                dp[i][j] = arr[i][j] + dp[i-1][j];
+                dp[i][j] = dp[i-1][j] + arr[i][j];
             }
-
-            // 왼쪽에서부터 누적합
+            // 오른쪽으로
             int[] left = new int[m];
             left[0] = dp[i][0];
             for(int j=1; j<m; j++){
-                left[j] = Math.max(dp[i][j], arr[i][j] + left[j-1]);
+                left[j] = Math.max(dp[i][j], left[j-1] + arr[i][j]);
             }
-
-            // 오른쪽부터 누적합
+            // 왼쪽으로
             int[] right = new int[m];
             right[m-1] = dp[i][m-1];
             for(int j=m-2; j>=0; j--){
-                right[j] = Math.max(dp[i][j], arr[i][j] + right[j+1]);
+                right[j] = Math.max(dp[i][j], right[j+1] + arr[i][j]);
             }
-
+            // 오/왼 비교
             for(int j=0; j<m; j++){
-                dp[i][j] = Math.max(left[j], right[j]);
+                dp[i][j] = Math.max(right[j], left[j]);
             }
         }
 
