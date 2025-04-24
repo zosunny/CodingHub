796bca2c20b5c1c26a9162f28a30377cb2b1a23d
rin <104357560+zosunny@users.noreply.github.com>
2025-04-24 13:12:52 +0900

    [level 3] Title: 입국심사, Time: 133.76 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 172.24 ms
+메모리: 94 MB, 시간: 133.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 14:17:58
+2025년 04월 24일 13:12:50
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,33 +1,41 @@
 import java.util.*;
 
+/*
+    심사 받는 시간 최소
+    사람 수 n <= 10억, 심사관 별 시간 times <= 100,000
+    1. 중복 조합 -> 절대 터짐
+    범위의 최솟값 -> 이분탐색
+    
+*/
+
 class Solution {
     
-    static long answer = Long.MAX_VALUE;;
     static int len;
+    static long answer = Long.MAX_VALUE;
     
-    public static void binarySearch(long start, long end, long n, int[] times){
-        if(start > end) return;
-        long mid = (start + end) / 2;
-        // 해당 예상 시간에 따른 처리 가능한 인원 수 계산
+    public static void binarySearch(long s, long e, int n, int[] times){
+        if(s > e) return;
+        long m = (s + e) / 2;
+        // 가능한 인원 계산
         long num = 0;
         for(int i=0; i<len; i++){
-            num += mid / times[i];
+            num += m / times[i];
         }
-        if(num >= n) {
-            answer = Math.min(answer, mid);
-            binarySearch(start, mid-1, n, times);
-        }
-        else {
-            binarySearch(mid+1, end, n, times);
+        // 가능한 인원이 총 인원보다 크면 시간 줄여도 됨
+        if(num >= n){
+            answer = Math.min(answer, m);
+            binarySearch(s, m-1, n, times);
+        }else{
+            binarySearch(m+1, e, n, times);
         }
     }
     
     public long solution(int n, int[] times) {
         
         len = times.length;
-        
         Arrays.sort(times);
-        binarySearch(1, (long)n*times[len-1], n, times);
+        
+        binarySearch(1L, (long)times[len-1] * n, n, times);
         
         return answer;
     }
