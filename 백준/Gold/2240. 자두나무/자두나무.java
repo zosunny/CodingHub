200229841cca43c74f414cb9766dfbece9cc22b9
rin <104357560+zosunny@users.noreply.github.com>
2025-04-24 17:37:25 +0900

    [Gold V] Title: 자두나무, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -19,16 +19,18 @@ public class Main {
         int[][] dp = new int[t+1][w+1];
         for(int i=1; i<t+1; i++){
             for(int j=0; j<w+1; j++){
-                dp[i][j] = dp[i-1][j];
-                if(j > 0) dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1]);
-                int idx = j % 2 == 0 ? 1 : 2;
-                if(idx == arr[i]) dp[i][j]++;
+                if(j == 0) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]);
+                int tmp = j % 2 == 0 ? 1 : 2;
+                if(tmp == arr[i]) dp[i][j]++;
             }
         }
+        
         int ans = 0;
-        for(int i=0; i<w+1; i++){
-            ans = Math.max(ans, dp[t][i]);
+        for(int j=0; j<w+1; j++){
+            ans = Math.max(ans, dp[t][j]);
         }
+
         System.out.println(ans);
     }
 }
