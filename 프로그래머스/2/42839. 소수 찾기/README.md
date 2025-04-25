c1df45909c3bffe31d0327fa7bee33994e673a10
rin <104357560+zosunny@users.noreply.github.com>
2025-04-25 21:03:06 +0900

    [level 2] Title: 소수 찾기, Time: 15.26 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 82.1 MB, 시간: 12.83 ms
+메모리: 98.7 MB, 시간: 15.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 20일 00:10:56
+2025년 04월 25일 21:03:04
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,63 +2,72 @@ import java.util.*;
 
 class Solution {
     
-    static int len;
+    static int n;
     static int answer;
-    static boolean[] select1;
-    static boolean[] select2;
-    static HashSet<Integer> set;
+    static int[] arr;
+    static Set<Integer> set;
     
-    public static boolean isPrime(int num){
-        if(num < 2) return false;
-        for(int i=2; i<(int)Math.sqrt(num)+1; i++){
-            if(num % i == 0) return false;
+    public static boolean isPrime(int x){
+        if(x < 2) return false;
+        for(int i=2; i<(int)Math.sqrt(x)+1; i++){
+            if(x % i == 0) return false;
         }
-        set.add(num);
         return true;
     }
     
-    public static void permu(int cnt, String[] arr, String[] input, int N){
-        if(cnt == N){
+    public static void permu(int cnt, int[] input, int s, List<Integer> list, boolean[] visit){
+        if(cnt == s){
             StringBuilder sb = new StringBuilder();
-            for(int i=0; i<N; i++){
-                sb.append(input[i]);
+            for(int i=0; i<s; i++){
+                if(visit[i]) sb.append(list.get(input[i]));
             }
-            if(sb.length() != 0) isPrime(Integer.parseInt(sb.toString()));
+            String str = sb.toString();
+            if(str.length() > 0 && isPrime(Integer.parseInt(str))){
+                set.add(Integer.parseInt(str));
+            }
             return;
         }
-        for(int i=0; i<N; i++){
-            if(select2[i]) continue;
-            input[cnt] = arr[i];
-            select2[i] = true;
-            permu(cnt+1, arr, input, N);
-            select2[i] = false;
+        for(int i=0; i<s; i++){
+            if(visit[i]) continue;
+            visit[i] = true;
+            input[cnt] = i;
+            permu(cnt+1, input, s, list, visit);
+            visit[i] = false;
         }
     }
     
-    public static void subset(int cnt, String numbers, int total){
-        if(cnt == len){
-            String[] arr = new String[total];
-            int idx = 0;
-            for(int i=0; i<len; i++){
-                if(select1[i]) arr[idx++] = numbers.substring(i, i+1);
+    public static void subset(int cnt, boolean[] select){
+        List<Integer> list = new ArrayList<>();
+        if(cnt == n){
+            for(int i=0; i<n; i++){
+                if(select[i]) {
+                    list.add(arr[i]);
+                }
             }
-            select2 = new boolean[total];
-            String[] input = new String [total];
-            permu(0, arr, input, total);
+            int s = list.size();
+            int[] input = new int[s];
+            boolean[] visit = new boolean[s];
+            permu(0, input, s, list, visit);
             return;
         }
-        select1[cnt] = true;
-        subset(cnt+1, numbers, total+1);
-        select1[cnt] = false;
-        subset(cnt+1, numbers, total);
+        select[cnt] = true;
+        subset(cnt+1, select);
+        select[cnt] = false;
+        subset(cnt+1, select);
     }
     
     public int solution(String numbers) {
+        n = numbers.length();
+        arr = new int[n];
         
-        len = numbers.length();
-        select1 = new boolean[len];
+        for(int i=0; i<n; i++){
+            arr[i] = numbers.charAt(i) - '0';
+        }
+        
         set = new HashSet<>();
-        subset(0, numbers, 0);
+        
+        boolean[] select = new boolean[n];
+        subset(0, select);
         
         return set.size();
     }
