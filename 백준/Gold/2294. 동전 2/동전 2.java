b52c6adad3bacef27c728016f8a57afaac0629b4
rin <104357560+zosunny@users.noreply.github.com>
2025-04-26 01:15:01 +0900

    [Gold V] Title: 동전 2, Time: 120 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -13,27 +13,19 @@ public class Main {
         int k = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-
         for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
 
         int[] dp = new int[k+1];
-        Arrays.fill(dp, Integer.MAX_VALUE - 1);
-
-        for(int i=0; i<k+1; i++){
+        Arrays.fill(dp, Integer.MAX_VALUE-1);
+        for(int i=1; i<k+1; i++){
             for(int j=0; j<n; j++){
                 if(i < arr[j]) continue;
                 if(i == arr[j]) dp[i] = 1;
                 else dp[i] = Math.min(dp[i], dp[i-arr[j]] + 1);
             }
         }
-
-        if(dp[k] == Integer.MAX_VALUE - 1){
-            System.out.println(-1);
-        }else{
-            System.out.println(dp[k]);
-        }
+        System.out.println(dp[k] == Integer.MAX_VALUE-1 ? -1 : dp[k]);
     }
 }
