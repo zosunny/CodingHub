e7a8fff11b4643200437ab49273b5ae8489c391c
rin <104357560+zosunny@users.noreply.github.com>
2025-04-26 02:39:25 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.88 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,41 +2,43 @@ import java.util.*;
 
 class Solution {
     
-    static int answer = Integer.MAX_VALUE;
     static int cnt = 1;
-    static boolean[] visited;
     static List<Integer>[] list;
+    static boolean[] visited;
     
     public static void dfs(int x){
         visited[x] = true;
         for(int i=0; i<list[x].size(); i++){
             int now = list[x].get(i);
             if(visited[now]) continue;
+            visited[now] = true;
             cnt++;
             dfs(now);
         }
     }
     
     public int solution(int n, int[][] wires) {
-        int len = wires.length;
+        int answer = Integer.MAX_VALUE;
         
-        for(int i=0; i<len; i++){
+        for(int k=0; k<n-1; k++){
             list = new ArrayList[n+1];
-            for(int k=0; k<n+1; k++){
-                list[k] = new ArrayList<>();
+            for(int i=0; i<n+1; i++){
+                list[i] = new ArrayList<>();
             }
+            visited = new boolean[n+1];
             cnt = 1;
-            for(int j=0; j<len; j++){
-                if(i == j) continue;
-                int x = wires[j][0];
-                int y = wires[j][1];
+            for(int i=0; i<n-1; i++){
+                int x = wires[i][0];
+                int y = wires[i][1];
+                if(k == i) continue;    // 전력망 1개 끊음
                 list[x].add(y);
                 list[y].add(x);
             }
-            visited = new boolean[n+1];
             dfs(1);
+            System.out.println();
             answer = Math.min(answer, Math.abs(n - 2 * cnt));
         }
+        
         return answer;
     }
 }
