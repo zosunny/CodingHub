316dc1a63f85806de45f1ff90f9bfb47b2043e53
rin <104357560+zosunny@users.noreply.github.com>
2025-04-25 18:21:34 +0900

    [level 2] Title: 숫자 변환하기, Time: 37.24 ms, Memory: 96.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        int[] dp = new int[y+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        dp[x] = 0;
+        for(int i=x; i<y+1; i++){
+            if(dp[i] == Integer.MAX_VALUE) continue;
+            if(i + n <= y) dp[i+n] = Math.min(dp[i+n], dp[i] + 1);
+            if(i * 2 <= y) dp[i*2] = Math.min(dp[i*2], dp[i] + 1);
+            if(i * 3 <= y) dp[i*3] = Math.min(dp[i*3], dp[i] + 1);
+        }
+        
+        return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
+    }
+}
