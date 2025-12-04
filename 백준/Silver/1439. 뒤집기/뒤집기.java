fc3813ba045040012cbf05cd65cdc445ee9a69cf
rin <104357560+zosunny@users.noreply.github.com>
2025-12-04 15:04:10 +0900

    [Silver V] Title: 뒤집기, Time: 136 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -6,18 +6,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] arr = br.readLine().toCharArray();
+        int len = arr.length;
+        int ans = 0;
 
-        int zero = 0;
-        int one = 0;
-        if(arr[0] == '0') zero++;
-        else one++;
-
-        for(int i=1; i<arr.length; i++){
-            if(arr[i-1] != arr[i]){
-                if(arr[i] == '0') zero++;
-                else one++;
+        while(true){
+            char c = arr[0];
+            int s = -1;
+            int e = -1;
+            for(int i=1; i<len; i++){
+                // 다른 숫자 발견, s 값이 아직 없으면
+                if(arr[i] != c && s == -1) s = i;
+                // 다른 숫자 발견할 때마다 e 갱신
+                if(arr[i] != c) e = i;
             }
+            if(s == -1) break;
+            // 뒤집기
+            for(int i=s; i<e+1; i++){
+                if(arr[i] == '0') arr[i] = '1';
+                else arr[i] = '0';
+            }
+            ans++;
         }
-        System.out.println(Math.min(zero, one));
+        System.out.println(ans);
     }
 }
