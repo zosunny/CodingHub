7b3278103d0a1fd5e9afbf6c0173b8da09f80897
rin <104357560+zosunny@users.noreply.github.com>
2025-12-06 14:54:32 +0900

    [Gold IV] Title: 최단경로, Time: 724 ms, Memory: 120024 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -5,36 +5,35 @@ import java.util.*;
 
 public class Main {
 
-    static class Point implements Comparable<Point>{
-        int y, d;
-        Point(int y, int d){
-            this.y = y;
-            this.d = d;
+    static int v, e;
+    static int s;
+    static List<Edge>[] list;
+    static int[] dis;
+
+    static class Edge implements Comparable<Edge>{
+        int idx, w;
+        Edge(int idx, int w){
+            this.idx = idx;
+            this.w = w;
         }
         @Override
-        public int compareTo(Point o){
-            int tmp = this.d - o.d;
-            if(tmp == 0) tmp = this.y - o.y;
-            return tmp;
+        public int compareTo(Edge o){
+            return this.w - o.w;
         }
     }
 
-    static int v, e;
-    static int start;
-    static List<Point>[] list;
-    static int[] dist;
-
-    public static void bfs(int x){
-        PriorityQueue<Point> q = new PriorityQueue<>();
-        q.add(new Point(x, 0));
-        while(!q.isEmpty()){
-            Point p = q.poll();
-            for(Point now : list[p.y]){
-                // 기존 보다 짧을 때만 추가
-                if((p.d + now.d) < dist[now.y]){
-                    dist[now.y] = p.d + now.d;
-                    q.add(new Point(now.y, p.d+now.d));
-                }
+    public static void dijk(int start){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[v+1];
+        pq.add(new Edge(start, 0));
+        dis[start] = 0;
+        while(!pq.isEmpty()){
+            Edge edge = pq.poll();
+            if(visited[edge.idx]) continue;
+            visited[edge.idx] = true;
+            for(Edge e : list[edge.idx]){
+                dis[e.idx] = Math.min(dis[e.idx], dis[edge.idx] + e.w);
+                pq.add(new Edge(e.idx, dis[e.idx]));
             }
         }
     }
@@ -45,8 +44,7 @@ public class Main {
 
         v = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
-
-        start = Integer.parseInt(br.readLine());
+        s = Integer.parseInt(br.readLine());
 
         list = new ArrayList[v+1];
         for(int i=0; i<v+1; i++){
@@ -55,30 +53,22 @@ public class Main {
 
         for(int i=0; i<e; i++){
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            list[x].add(new Point(y, d));
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            list[s].add(new Edge(e, w));
         }
 
-        // 거리를 기준으로 오름차순 정렬
-        for(int i=0; i<v+1; i++){
-            Collections.sort(list[i]);
-        }
+        dis = new int[v+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dijk(s);
 
-        // 각 거리 초기화
-        dist = new int[v+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
-
-        bfs(start);
-
-        // 출력
         StringBuilder sb = new StringBuilder();
         for(int i=1; i<v+1; i++){
-            if(dist[i] == Integer.MAX_VALUE) sb.append("INF" + "\n");
-            else sb.append(dist[i] + "\n");
+            if(dis[i] == Integer.MAX_VALUE) sb.append("INF" + "\n");
+            else sb.append(dis[i] + "\n");
         }
+
         System.out.println(sb.toString());
     }
 }
