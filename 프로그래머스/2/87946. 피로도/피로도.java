45021810864087a9c36f8f14c9def6fb94db5d6d
rin <104357560+zosunny@users.noreply.github.com>
2026-02-27 16:43:14 +0900

    [level 2] Title: 피로도, Time: 7.66 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,30 @@
 import java.util.*;
 
+/*
+탐험할 수 있는 최대 던전 수
+
+1. 순열
+2. 탐색(max값)
+-> 8!
+*/
+
 class Solution {
     
-    static int answer;
     static int len;
-    static boolean[] select;
+    static int ans;
     
-    // 순서대로 탐험했을 때 탐험 가능한 던전 수 계산하는 메서드
     public static void calc(int[] input, int k, int[][] dungeons){
-        int cnt = 0;
+        int tmp = 0;
         for(int i=0; i<len; i++){
-            int d = input[i];
-            // 현재 피로도보다 최소 필요 피로도가 작아야 함
-            if(k < dungeons[d][0]) continue;
-            k -= dungeons[d][1];
-            cnt++;
+            // 현재 피로도 < 최소 필요 피로도
+            if(k < dungeons[input[i]][0]) break;
+            k -= dungeons[input[i]][1];
+            tmp++;
         }
-        answer = Math.max(answer, cnt);
+        ans = Math.max(ans, tmp);
     }
     
-    // 탐험할 던전의 순서 정하는 메서드
-    public static void permu(int cnt, int[] input, int k, int[][] dungeons){
+    public static void permu(int cnt, int[] input, boolean[] select, int k, int[][] dungeons){
         if(cnt == len){
             calc(input, k, dungeons);
             return;
@@ -29,18 +33,18 @@ class Solution {
             if(select[i]) continue;
             input[cnt] = i;
             select[i] = true;
-            permu(cnt+1, input, k, dungeons);
+            permu(cnt+1, input, select, k, dungeons);
             select[i] = false;
         }
     }
     
     public int solution(int k, int[][] dungeons) {
-        
         len = dungeons.length;
-        int[] input = new int[len];
-        select = new boolean[len];
-        permu(0, input, k, dungeons);
         
-        return answer;
+        int[] input = new int[len];
+        boolean[] select = new boolean[len];
+        permu(0, input, select, k, dungeons);
+        
+        return ans;
     }
 }
