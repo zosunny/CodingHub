ada7f5a14ade2adcd0ea3ace4b007b6ee178fba6
rin <104357560+zosunny@users.noreply.github.com>
2026-02-28 00:49:50 +0900

    [level 2] Title: 올바른 괄호, Time: 14.51 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,10 +1,5 @@
 import java.util.*;
 
-/*
-    ()를 계속 찾아서 없앴을 때 남아 있는 게 있으면 false
-    문자열 매번 복사 * 새로 생성 -> 최대 100,000 * 최대 100,000번 반복 -> O(100,000^2)
-*/
-
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
@@ -13,15 +8,15 @@ class Solution {
         
         for(int i=0; i<n; i++){
             char c = s.charAt(i);
-            if(c == '(') st.push(c);
-            else {
+            if(c =='(') st.push(c);
+            else{
                 if(st.isEmpty()) return false;
                 st.pop();
             }
         }
         
-        if(!st.isEmpty()) answer = false;
-        
+        if(!st.isEmpty()) return false;
+
         return answer;
     }
 }
