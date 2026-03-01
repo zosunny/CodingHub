dc1447cba1e2ea7db22ed728bdd6707aa372d753
rin <104357560+zosunny@users.noreply.github.com>
2026-03-01 13:05:38 +0900

    [level 3] Title: 베스트앨범, Time: 4.96 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,45 +1,47 @@
 import java.util.*;
 
-/*
-    장르 -> 많은 재생 횟수 -> 낮은 고유 번호
-    장르별 2개만
-    고유 번호 반환
-    
-    1) {장르: 재생횟수} 저장
-    2) {장르: List<int[]> 고유번호, 재생횟수}
-*/
-
 class Solution {
+    
+    static class Point{
+        int num;
+        int cnt;
+        Point(int num, int cnt){
+            this.num = num;
+            this.cnt = cnt;
+        }
+    }
+    
     public int[] solution(String[] genres, int[] plays) {
         int n = genres.length;
         
+        // 1) 장르별 정보 저장
         Map<String, Integer> map = new HashMap<>();
-        Map<String, List<int[]>> mapList = new HashMap<>();
+        Map<String, List<Point>> mapList = new HashMap<>();
         for(int i=0; i<n; i++){
             String g = genres[i];
             int p = plays[i];
-            // 장르별 재생 총합 정보 저장
+            // 총 재생횟수
             map.put(g, map.getOrDefault(g, 0) + p);
-            // 개별 곡 재생 정보 저장
+            // 개별 곡 저장
             mapList.putIfAbsent(g, new ArrayList<>());
-            mapList.get(g).add(new int[]{i, p});
+            mapList.get(g).add(new Point(i, p));
         }
         
-        // map 정렬 -> List
+        // 2) 장르별 총 재생횟수 내림차순
         List<String> keySet = new ArrayList<>(map.keySet());
         keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
         
-        // 해당 장르에서 수록 기준에 따른 고유 번호 저장
+        // 3) 해당 장르에서의 개별 곡 재생횟수 내림차순, 고유번호 오름차순
         List<Integer> ans = new ArrayList<>();
         for(String s : keySet){
-            List<int[]> tmp = mapList.get(s);
+            List<Point> tmp = mapList.get(s);
             tmp.sort((o1, o2) -> {
-                if(o1[1] == o2[1]) return o1[0] - o2[0];
-                else return o2[1] - o1[1];
+                if(o1.cnt == o2.cnt) return o1.num - o2.num;
+                else return o2.cnt - o1.cnt;
             });
-            // 곡이 한개인 경우도 고려
+            // 2개씩 저장하되, 곡이 1개인 경우도 고려
             for(int i=0; i<tmp.size() && i<2; i++){
-                ans.add(tmp.get(i)[0]);
+                ans.add(tmp.get(i).num);
             }
         }
         
