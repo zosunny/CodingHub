706809ba993492c69baedb1b84147ad9caf70e55
rin <104357560+zosunny@users.noreply.github.com>
2026-03-04 18:00:46 +0900

    [level 2] Title: 타겟 넘버, Time: 7.73 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,40 +1,27 @@
 import java.util.*;
 
+/*
+    -, + 순열 -> 최대 2^20
+*/
+
 class Solution {
     
     static int n;
-    static int answer;
+    static int ans;
     
-    public static void calc(int[] input, int[] numbers, int target){
-        int tmp = 0;
-        for(int i=0; i<n; i++){
-            if(input[i] == 0){
-                tmp += numbers[i];
-            }else{
-                tmp -= numbers[i];
-            }
-        }
-        if(tmp == target) answer++;
-    }
-    
-    public static void permu(int cnt, int[] input, int[] numbers, int target){
+    public static int dfs(int cnt, int sum, int[] numbers, int target){
         if(cnt == n){
-            calc(input, numbers, target);
-            return;
+            return sum == target ? ans+1 : ans;
         }
-        for(int i=0; i<2; i++){
-            input[cnt] = i;
-            permu(cnt+1, input, numbers, target);
-        }
+        return dfs(cnt+1, sum+numbers[cnt], numbers, target)
+            + dfs(cnt+1, sum-numbers[cnt], numbers, target);
     }
     
     public int solution(int[] numbers, int target) {
-        
         n = numbers.length;
         
-        int[] input = new int[n];
-        permu(0, input, numbers, target);
+        ans = dfs(0, 0, numbers, target);
         
-        return answer;
+        return ans;
     }
 }
