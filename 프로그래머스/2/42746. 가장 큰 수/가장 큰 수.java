11dabce71562bb0735592a225ce1ca1391130ea9
rin <104357560+zosunny@users.noreply.github.com>
2026-03-04 21:57:17 +0900

    [level 2] Title: 가장 큰 수, Time: 201.35 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,27 +1,23 @@
 import java.util.*;
 
-/*
-    1. 순열 -> 100,000! X
-    2. 비교정렬 -> nlogn?
-*/
-
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
         int n = numbers.length;
-        String[] arr = new String[n];
         
+        String[] arr = new String[n];
         for(int i=0; i<n; i++){
             arr[i] = Integer.toString(numbers[i]);
         }
         
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Arrays.sort(arr, (o1, o2) -> (o2+o1).compareTo(o1+o2));
         
+        if(arr[0].equals("0")) return "0";
+        
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++){
-            answer += arr[i];
-            if(answer.equals("00")) answer = "0";
+            sb.append(arr[i]);
         }
         
-        return answer;
+        return sb.toString();
     }
 }
