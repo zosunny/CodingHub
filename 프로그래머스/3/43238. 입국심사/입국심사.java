091ad3d7dddd4b72eb48539557e2604a05ebfbfc
rin <104357560+zosunny@users.noreply.github.com>
2026-03-04 21:20:18 +0900

    [level 3] Title: 입국심사, Time: 105.07 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,32 +1,38 @@
 import java.util.*;
 
 /*
-    심사 받는 시간 최소
-    사람 수 n <= 10억, 심사관 별 시간 times <= 100,000
-    1. 중복 조합 -> 절대 터짐
-    범위의 최솟값 -> 이분탐색
+    - 한 심사대 = 심사 1명
+    - 최소 심사 시간
+    - n <= 10억, 심사시간 <= 10억
     
+    1. 시간범위 0분~최대 max(times) * n
+    2. 이분탐색으로 해당 시간동안 처리 가능한 최대 사람 수 : time/[0] + ... + time/times[i]
+    -> 최대 log10억인데 심사시간은 long!!!
 */
 
 class Solution {
     
     static int len;
-    static long answer = Long.MAX_VALUE;
+    static long ans = Long.MAX_VALUE;
+    
+    public static long calc(long mid, int[] times){
+        long cnt = 0;
+        for(int i=0; i<len; i++){
+            cnt += mid / times[i];
+        }
+        return cnt;
+    }
     
     public static void binarySearch(long s, long e, int n, int[] times){
         if(s > e) return;
-        long m = (s + e) / 2;
-        // 가능한 인원 계산
-        long num = 0;
-        for(int i=0; i<len; i++){
-            num += m / times[i];
-        }
-        // 가능한 인원이 총 인원보다 크면 시간 줄여도 됨
-        if(num >= n){
-            answer = Math.min(answer, m);
-            binarySearch(s, m-1, n, times);
+        
+        long mid = (s + e) / 2;
+        
+        if(calc(mid, times) >= n){
+            ans = Math.min(ans, mid);
+            binarySearch(s, mid-1, n, times);
         }else{
-            binarySearch(m+1, e, n, times);
+            binarySearch(mid+1, e, n, times);
         }
     }
     
@@ -35,8 +41,8 @@ class Solution {
         len = times.length;
         Arrays.sort(times);
         
-        binarySearch(1L, (long)times[len-1] * n, n, times);
+        binarySearch(1L, (long)times[len-1]*n, n, times);
         
-        return answer;
+        return ans;
     }
 }
