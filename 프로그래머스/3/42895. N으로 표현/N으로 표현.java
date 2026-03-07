1c6174b681fc257bdb87c921c2018d04e11f1656
rin <104357560+zosunny@users.noreply.github.com>
2026-03-07 11:02:21 +0900

    [level 3] Title: N으로 표현, Time: 18.65 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -9,46 +9,49 @@ import java.util.*;
     i=3) 555, dp[1]+dp[2], dp[1]-dp[2], dp[1]*dp[2], dp[1]/dp[2],
               dp[2]+dp[1], dp[2]-dp[1], dp[2]*dp[1], dp[2]/dp[1]
     -> 근데 중복값 있을 수 있어서 set 사용
-    ** -> 0으로 나누는 경우 고려
+    ** -> 0은 들어갈 수 없음(0으로 나눌 수 없음, 1이상임)
+    ** -> 32000 이하만 저장하면 됨(N이 32000이하)
 */
 
 class Solution {
     public int solution(int N, int number) {
         
-        if(number == N) return 1;
+        if(N == number) return 1;
         
         List<Set<Integer>> list = new ArrayList<>();
-        for(int i=0; i<10; i++){
+        for(int i=0; i<9; i++){
             list.add(new HashSet<>());
         }
         
         list.get(1).add(N);
         
-        for(int i=2; i<10; i++){
-            // 합친 값
-            StringBuilder sb = new StringBuilder();
+        for(int i=2; i<9; i++){
+            // 붙여서 사용하는 경우
+            String str = "";
             for(int j=0; j<i; j++){
-                sb.append(N);
+                str += Integer.toString(N);
             }
-            list.get(i).add(Integer.parseInt(sb.toString()));
+            list.get(i).add(Integer.parseInt(str));
             
-            // 이전 값 연산
+            // 이전값으로 사칙연산 하는 경우
             for(int j=1; j<i; j++){
                 for(int x : list.get(j)){
                     for(int y : list.get(i-j)){
-                        int x1 = x + y;
-                        int x2 = x - y;
-                        int x3 = x * y;
-                        int x4 = x / y;
-                        if(x1!=0 && x1<=32000) list.get(i).add(x1);
-                        if(x2!=0 && x2<=32000) list.get(i).add(x2);
-                        if(x3!=0 && x3<=32000) list.get(i).add(x3);
-                        if(x4!=0 && x4<=32000) list.get(i).add(x4);
+                        int n1 = x + y;
+                        int n2 = x - y;
+                        int n3 = x * y;
+                        int n4 = x / y;
+                        if(n1 > 0 && n1 <= 32000) list.get(i).add(n1);
+                        if(n2 > 0 && n2 <= 32000) list.get(i).add(n2);
+                        if(n3 > 0 && n3 <= 32000) list.get(i).add(n3);
+                        if(n4 > 0 && n4 <= 32000) list.get(i).add(n4);
                     }
                 }
-                if(list.get(i).contains(number)) return i;
             }
+            if(list.get(i).contains(number)) return i;
         }
+        
+        
         return -1;
     }
 }
