014a0303bca2d4766a2c697c672507b9168e415b
rin <104357560+zosunny@users.noreply.github.com>
2026-03-19 00:08:02 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 128 ms, Memory: 15272 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -7,44 +7,40 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
+        for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[n];
+        int[] dp = new int[N];
         Arrays.fill(dp, 1);
 
-        int len = 1;
-        for(int i=1; i<n; i++){
+        int lis = 1;
+        for(int i=1; i<N; i++){
             for(int j=0; j<i; j++){
-                if(arr[j] < arr[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    len = Math.max(len, dp[i]);
-                }
+                if(arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+                lis = Math.max(lis, dp[i]);
             }
         }
+        sb.append(lis).append("\n");
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(len).append("\n");
-
-        // 스택으로 역추적
         Stack<Integer> s = new Stack<>();
-        for(int i=n-1; i>=0; i--){
-            if(dp[i] == len) {
+        for(int i=N-1; i>=0; i--){
+            if(dp[i] == lis){
                 s.push(arr[i]);
-                len--;
+                lis--;
             }
         }
 
         while(!s.isEmpty()){
             sb.append(s.pop() + " ");
         }
-
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
