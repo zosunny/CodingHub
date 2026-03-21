a22e1d8aa4418491409b14c4ea42fb546eaafd79
rin <104357560+zosunny@users.noreply.github.com>
2026-03-21 21:50:24 +0900

    [Silver I] Title: 안전 영역, Time: 220 ms, Memory: 18776 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,72 +1,58 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int maxHigh = Integer.MIN_VALUE;
-	static int maxCnt = Integer.MIN_VALUE;
-	
-	static int[][] arr;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, 1, -1};
-	
-	public static int bfs(int x, int y) {
-		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(x, y));
-		visited[x][y] = true;
-		while(!q.isEmpty()) {
-			Point p = q.poll();
-			for(int i=0; i<4; i++) {
-				int nx = p.x + dx[i];
-				int ny = p.y + dy[i];
-				if(nx<0 || ny<0 || nx>=N || ny>=N || visited[nx][ny] || map[nx][ny]==1) continue;
-				q.add(new Point(nx, ny));
-				visited[nx][ny] = true;
-			}
-		}
-		return 1;
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-		arr = new int[N][N];
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				maxHigh = Math.max(maxHigh, arr[i][j]);			// 영역의 최대 높이
-			}
-		}
-		for(int h=0; h<maxHigh; h++) {		// 비의 양
-			map = new int[N][N];			// 맵 초기화
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(arr[i][j]<=h) {
-						map[i][j] = 1;		// 잠기는 부분을 1로 표시
-					}
-				}
-			}
-			visited = new boolean[N][N];
-			int cnt = 0;
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(!visited[i][j] && map[i][j]==0) {	// 방문하지 않고 잠기지 않은 부분
-						cnt += bfs(i, j);
-					}
-				}
-			}
-			maxCnt = Math.max(maxCnt, cnt);		// 매 높이의 비의 양마다 안전영역 개수 갱신
-		}
-		System.out.println(maxCnt);
-	}
+
+    static int n;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] arr;
+    static boolean[][] visited;
+
+    public static void dfs(int x, int y, int limit){
+        visited[x][y] = true;
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx<0 || ny<0 || nx>=n || ny>=n || visited[nx][ny]) continue;
+            if(arr[nx][ny] <= limit) continue;
+            dfs(nx, ny, limit);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n][n];
+        int maxH = 0;
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                maxH = Math.max(maxH, arr[i][j]);
+            }
+        }
+
+        // 1~maxH까지 물에 잠길 때 안전 영역 갱신
+        int ans = 0;
+        for(int h=0; h<maxH; h++){
+            visited = new boolean[n][n];
+            int tmp = 0;
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if(visited[i][j]) continue;
+                    if(arr[i][j] > h) {
+                        dfs(i, j, h);
+                        tmp++;
+                    }
+                }
+            }
+            ans = Math.max(ans, tmp);
+        }
+        System.out.println(ans);
+    }
 }
