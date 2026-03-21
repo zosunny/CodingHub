6de3b786e26b88061cc036735fb4e306efafb9f3
rin <104357560+zosunny@users.noreply.github.com>
2026-03-21 14:10:25 +0900

    [Gold II] Title: 반도체 설계, Time: 252 ms, Memory: 21608 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for(int i=1; i<n; i++){
+            if(arr[i] > list.get(list.size()-1)) list.add(arr[i]);
+            else{
+                int s = 0;
+                int e = list.size() - 1;
+                while(s < e){
+                    int mid = (s + e) / 2;
+                    if(arr[i] > list.get(mid)) s = mid + 1;
+                    else e = mid;
+                }
+                list.set(s, arr[i]);
+            }
+        }
+        System.out.println(list.size());
+    }
+}
