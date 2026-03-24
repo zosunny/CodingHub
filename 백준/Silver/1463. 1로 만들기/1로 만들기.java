732b2f6853bcbf55c5b92dcb9fcff24e1eb6b275
rin <104357560+zosunny@users.noreply.github.com>
2026-03-24 11:39:03 +0900

    [Silver III] Title: 1로 만들기, Time: 204 ms, Memory: 21724 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,30 +2,23 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
-		int N = sc.nextInt();
-		int[] arr = new int[N + 1];
-		
-		// 배열값 초기화
-		for(int i=2; i<arr.length; i++) {
-			
-			int tmp1 = Integer.MAX_VALUE;
-			int tmp2 = Integer.MAX_VALUE;
-			int tmp3 = Integer.MAX_VALUE;
-			
-			if (i % 3 == 0) {
-				tmp1 = arr[i/3] + 1;
-			}
-			if (i % 2 == 0) {
-				tmp2 = arr[i/2] + 1;
-			}
-			tmp3 = arr[i-1] + 1;
-			
-			arr[i] = Math.min(tmp3, Math.min(tmp2, tmp1));
-		}
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-		System.out.println(arr[N]);
-	}
+        int[] dp = new int[N+3];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for(int i=4; i<N+1; i++){
+            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+            if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            dp[i] = Math.min(dp[i], dp[i-1] + 1);
+        }
+
+        System.out.println(dp[N]);
+    }
 }
